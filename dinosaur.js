c123f8490b4fb415951ec753d1d6f60d7ca0004f
LouiseVen <louise.venuleth@gmail.com>
2023-05-26 08:23:20 +0200

    Tried to debug the animations

--- dinosaur.js
+++ dinosaur.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 // Models
 
-const dinoUrl = new URL('assets/Parasaurolophus.gltf', import.meta.url);
+const dinoUrl = new URL("assets/Parasaurolophus.gltf", import.meta.url);
 
 // Initialization
 
@@ -36,10 +36,9 @@ scene.add(axesHelper);
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
-
 // Objects
 
-    //Plane
+//Plane
 
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
 const planeMaterial = new THREE.MeshLambertMaterial({
@@ -51,26 +50,31 @@ scene.add(plane);
 plane.rotation.x = -0.5 * Math.PI;
 plane.receiveShadow = true;
 
-    //Dino
+//Dino
 
 const assetLoader = new GLTFLoader();
-assetLoader.load(dinoUrl.href, function(gltf){
+assetLoader.load(
+  dinoUrl.href,
+  function (gltf) {
     const dino = gltf.scene;
     scene.add(dino);
-    dino.position.set(0,0,0);
-    dino.scale.set(0.01,0.01,0.01);
-}, undefined, function(error){
+    dino.position.set(0, 0, 0);
+    dino.scale.set(0.01, 0.01, 0.01);
+  },
+  undefined,
+  function (error) {
     console.error(error);
-})
+  }
+);
 
-    // Light
+// Light
 
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
 scene.add(directionalLight);
-directionalLight.position.x = 30
+directionalLight.position.x = 30;
 directionalLight.position.y = 20;
 directionalLight.castShadow = true;
 directionalLight.shadow.camera.top = 12;
@@ -78,15 +82,36 @@ directionalLight.shadow.camera.top = 12;
 const dLightHelper = new THREE.DirectionalLightHelper(directionalLight);
 scene.add(dLightHelper);
 
-let step =1;
+let step = 1;
 let speed = 0.01;
 
-function animate(){
+let mixer;
+let Player_anim_IDLE;
+let Player_anim_RUN;
 
+let loader = new GLTFLoader();
+loader.load(dinoUrl.href, function (gltf) {
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  Player_anim_IDLE = gltf.animations[0]; // first animation
+  Player_anim_RUN = gltf.animations[1]; // second animation
 
-    step+=speed;
+  mixer.clipAction( Player_anim_IDLE).play();
 
-    renderer.render(scene,camera);
+  MainPlayer = gltf.scene;
+    scene.add(MainPlayer);
+});
+
+function animate() {
+  step += speed;
+
+  renderer.render(scene, camera);
 }
 
-renderer.setAnimationLoop(animate);
+function animation()
+{
+  
+  var delta = clock.getDelta();
+  mixer.update( delta );
+}
+
+renderer.setAnimationLoop(animation);
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import * as dat from 'dat.gui';
+import * as dat from "dat.gui";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-const dinoUrl = new URL('assets/Parasaurolophus.gltf', import.meta.url);
-const monkeyUrl = new URL('assets/monkey.glb', import.meta.url);
+const dinoUrl = new URL("assets/Parasaurolophus.gltf", import.meta.url);
+const dinoUrlFBX = new URL("assets/Parasaurolophus.fbx", import.meta.url);
+const monkeyUrl = new URL("assets/monkey.glb", import.meta.url);
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -52,8 +53,8 @@ plane.receiveShadow = true;
 const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
-const sphereGeometry = new THREE.SphereGeometry;
-const sphereMaterial = new THREE.MeshStandardMaterial ({color: 0x0000FF});
+const sphereGeometry = new THREE.SphereGeometry();
+const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 sphere.position.x = 2;
 sphere.castShadow = true;
@@ -62,9 +63,9 @@ scene.add(sphere);
 const ambientLight = new THREE.AmbientLight(0x333333);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
 scene.add(directionalLight);
-directionalLight.position.x = 30
+directionalLight.position.x = 30;
 directionalLight.position.y = 20;
 directionalLight.castShadow = true;
 directionalLight.shadow.camera.top = 12;
@@ -72,39 +73,65 @@ directionalLight.shadow.camera.top = 12;
 const dLightHelper = new THREE.DirectionalLightHelper(directionalLight);
 scene.add(dLightHelper);
 
-const gui = new dat.GUI(); 
+const gui = new dat.GUI();
 
 const options = {
-    sphereColor: '#ffea00',
-    wireframe: false,
+  sphereColor: "#ffea00",
+  wireframe: false,
 };
 
-gui.addColor(options, 'sphereColor').onChange(function(e){
-    sphere.material.color.set(e);
+gui.addColor(options, "sphereColor").onChange(function (e) {
+  sphere.material.color.set(e);
 });
 
-gui.add(options, 'wireframe').onChange(function(e){
-    sphere.material.wireframe = e;
-})
+gui.add(options, "wireframe").onChange(function (e) {
+  sphere.material.wireframe = e;
+});
 
 let step = 0;
 let speed = 0.01;
 
 const assetLoader = new GLTFLoader();
-assetLoader.load(dinoUrl.href, function(gltf){
-    const model = gltf.scene;
-    scene.add(model);
-    model.position.set(-12,4,10);
-    model.scale.set(0.01,0.01,0.01);
-}, undefined, function(error){
+assetLoader.load(
+  dinoUrl.href,
+  function (gltf) {
+    console.log(gltf.animations);
+  },
+  undefined,
+  function (error) {
     console.error(error);
-})
+  }
+);
+
+let mixer;
+let Player_anim_IDLE;
+let Player_anim_RUN;
+
+const assetFBX = new FBXLoader();
+assetFBX.load(
+  dinoUrlFBX.href,
+  function (fbx) {
+    console.log(fbx.animations);
+    mixer = new THREE.AnimationMixer(fbx.scene);
+    Player_anim_IDLE = fbx.animations[0]; // first animation
+    Player_anim_RUN = fbx.animations[1]; // second animation
+
+    mixer.clipAction(Player_anim_IDLE).play();
+
+    MainPlayer = fbx.scene;
+    scene.add(MainPlayer);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
 function animate() {
   box.rotation.x += 0.01;
   box.rotation.y += 0.01;
   step += speed;
-  sphere.position.y =10*Math.abs(Math.sin(step));
+  sphere.position.y = 10 * Math.abs(Math.sin(step));
   renderer.render(scene, camera);
 }
 
